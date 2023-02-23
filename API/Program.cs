141acafa3207521329680e0d82ecdd86fec8d5ec
Tea2006 <teodora2222222@gmail.com>
2023-02-23 19:43:52 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 /* builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); */
@@ -27,6 +28,7 @@ var app = builder.Build();
 
 //app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
